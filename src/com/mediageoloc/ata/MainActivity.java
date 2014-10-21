d92c06cff7e4222676b959f7826996fdb92b8c36
Moea-Chan <audeakrich17@hotmail.fr>
2014-10-21 15:10:09 +0200

    en cours sur les menus contextuels

--- src/com/mediageoloc/ata/MainActivity.java
+++ src/com/mediageoloc/ata/MainActivity.java
@@ -1,18 +1,82 @@
 package com.mediageoloc.ata;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
 
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class MainActivity extends Activity {
 
+	private Button _buttonPhoto;
+	private Button _buttonVideo;
+	private Button _buttonAudio;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		addButtonPhotoListener();
+		addButtonAudioListener();
+		addButtonVideoListener();
+		Menufinal menuItemVideo = (Button) findViewById(R.id.buttonPhoto);
+		_buttonPhoto.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	showPopup(v);
+            }
+        });
 	}
-
+	
+	public void showPopup(View v) {
+		Button buttonClicked = (Button)v;
+		PopupMenu popup = new PopupMenu(this, v);
+	    MenuInflater inflater = popup.getMenuInflater();
+	    int clickedButtonId = R.menu.capture_options_photo;
+		
+		if(buttonClicked == findViewById(R.id.buttonAudio)){
+			clickedButtonId = R.menu.capture_options_audio;
+		}
+		else if(buttonClicked == findViewById(R.id.buttonVideo)){
+			clickedButtonId = R.menu.capture_options_video;
+		}
+		inflater.inflate(clickedButtonId, popup.getMenu());
+	    popup.show();
+	}
+	/*
+	 * Ajout 
+	 */
+	private void addButtonPhotoListener(){
+		_buttonPhoto = (Button) findViewById(R.id.buttonPhoto);
+		_buttonPhoto.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	showPopup(v);
+            }
+        });
+	}
+	
+	private void addButtonVideoListener(){
+		_buttonVideo = (Button) findViewById(R.id.buttonVideo);
+		_buttonVideo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	showPopup(v);
+            }
+        });
+	}
+	
+	private void addButtonAudioListener(){
+		_buttonAudio = (Button) findViewById(R.id.buttonAudio);
+		_buttonAudio.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	showPopup(v);
+            }
+        });
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
