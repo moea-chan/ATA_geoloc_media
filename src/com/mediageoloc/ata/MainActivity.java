d8f96720e708ad2667852fbcf4c69335589ad6fe
Moea-Chan <audeakrich17@hotmail.fr>
2014-10-22 10:32:53 +0200

    good version

--- src/com/mediageoloc/ata/MainActivity.java
+++ src/com/mediageoloc/ata/MainActivity.java
@@ -5,12 +5,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.PopupMenu;
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class MainActivity extends Activity {
@@ -32,29 +28,7 @@ public class MainActivity extends Activity {
 
 
 	}
-	/*
-	 * Display contextual menu on button
-	 */
-	public void showPopup(View v) {
-		//get clicked button
-		Button buttonClicked = (Button)v;
-		PopupMenu popup = new PopupMenu(this, v);
-	    MenuInflater inflater = popup.getMenuInflater();
-	    
-	    //default : photo
-	    int clickedButtonId = R.menu.capture_options_photo;
-		//if audio
-		if(buttonClicked == findViewById(R.id.buttonAudio)){
-			clickedButtonId = R.menu.capture_options_audio;
-		}
-		//if video
-		else if(buttonClicked == findViewById(R.id.buttonVideo)){
-			clickedButtonId = R.menu.capture_options_video;
-		}
-		
-		inflater.inflate(clickedButtonId, popup.getMenu());
-	    popup.show();
-	}
+	
 	
 	private void addButtonPhotoListener(){
 		_buttonPhoto = (Button) findViewById(R.id.buttonPhoto);
@@ -64,9 +38,6 @@ public class MainActivity extends Activity {
             	Intent intent = new Intent(MainActivity.this,
 						PhotoActivity.class);
 				startActivity(intent);
-
-            	
-
             }
         });
 	}
@@ -90,30 +61,5 @@ public class MainActivity extends Activity {
 	}
 	
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.from_gallery_photo) {
-			return true;
-		}
-		else if (id == R.id.from_snapshot_photo) {
-			return true;
-		}
-		else if (id == R.id.from_gallery_video) {
-			return true;
-		}
-		else if (id == R.id.from_snapshot_video) {
-			return true;
-		}
-		else if (id == R.id.from_gallery_audio) {
-			return true;
-		}
-		else if (id == R.id.from_snapshot_audio) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+	
 }
