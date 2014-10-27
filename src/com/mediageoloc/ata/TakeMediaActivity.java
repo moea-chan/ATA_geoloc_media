600919d11f3754be03d9a24197edacce9b1b88fb
Moea-Chan <audeakrich17@hotmail.fr>
2014-10-27 15:26:12 +0100

    intégration commentaire enregistrement historique drawer à finir

--- src/com/mediageoloc/ata/TakeMediaActivity.java
+++ src/com/mediageoloc/ata/TakeMediaActivity.java
@@ -7,6 +7,8 @@ import java.util.Date;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -17,7 +19,7 @@ import android.view.View;
 import android.widget.Button;
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-public class TakeMediaActivity extends Activity {
+public class TakeMediaActivity extends DrawerActivity {
 	
 
 	private Button _buttonPhoto;
@@ -34,6 +36,8 @@ public class TakeMediaActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_take_media);
+		
+		initHistoriquePreferences();
 		
 		// add listeners on buttons
 		addButtonPhotoListener();
@@ -120,8 +124,45 @@ public class TakeMediaActivity extends Activity {
 		_buttonAudio.setOnClickListener(new View.OnClickListener() {
             @Override
 			public void onClick(View v) {
+            	startHistoricMediaActivity();
             }
         });
+	}
+	
+	private void startHistoricMediaActivity(){
+		Intent intent = new Intent(this, HistoricMediaActivity.class);
+        startActivity(intent);
+	}
+	
+	/**
+	 * initHistoriquePreferences()
+	 * 
+	 * -Verifie l'existence de l'historiquePreferences et le complete eventuellement
+	 * 		avec filePathX pour les URI et commentaireX pour les commentaires (0 < X < 11)
+	 */
+	private void initHistoriquePreferences(){
+		
+		SharedPreferences sharedpreferences  =  getSharedPreferences("historiquePreferences", MODE_PRIVATE);
+		
+		String existHisto = sharedpreferences.getString("filePath10", "%&%p%&defValue");
+		String stKey;
+		String stValue = "";
+		
+		if (existHisto.equals("%&%p%&defValue"))
+		{
+			//Historique inexistant => Création
+			
+			Editor editor = sharedpreferences.edit();
+			for (int i =1;i<11;i++)
+			{
+				stKey= "filePath" + String.valueOf(i);
+				editor.putString(stKey,stValue);
+				stKey= "commentaire" + String.valueOf(i);
+				editor.putString(stKey,stValue);
+			}
+			editor.commit();
+		}		
+		
 	}
 
 }
