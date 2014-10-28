d300c7c8c219edd558b88e58255b0765e4d1f5fd
Moea-Chan <audeakrich17@hotmail.fr>
2014-10-28 16:15:29 +0100

    externalisation des fonctions statiques de gestion des préférences et du traitement des photos

--- src/com/mediageoloc/ata/media/photo/PhotoCommentPreviewActivity.java
+++ src/com/mediageoloc/ata/media/photo/PhotoCommentPreviewActivity.java
@@ -2,23 +2,18 @@ package com.mediageoloc.ata.media.photo;
 
 
 
-import java.io.IOException;
-
-
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.mediageoloc.ata.R;
+import com.mediageoloc.ata.historic.HistoricPrefManager;
 import com.mediageoloc.ata.media.TakeMediaActivity;
 
 public class PhotoCommentPreviewActivity extends Activity {
@@ -38,7 +33,7 @@ public class PhotoCommentPreviewActivity extends Activity {
 		Intent intent = getIntent();
 		ImageView imageView = (ImageView) findViewById(R.id.photo_preview);
 		editTextComment = (EditText) findViewById(R.id.edit_text_comment);
-		
+		photoUri = Uri.parse(intent.getStringExtra("photoUri"));
 		Bitmap bitmap = PhotoUtils.loadPhotoInImageViewByUri(this.getContentResolver(), intent.getStringExtra("photoUri"), 190, 140);
 		imageView.setImageBitmap(bitmap);
 		
@@ -55,53 +50,53 @@ public class PhotoCommentPreviewActivity extends Activity {
 	}
 	
 	private void savePictureCommented(){
-		gestionHistoriquePreferences();
+		HistoricPrefManager.addHistoriquePreferences(photoUri, editTextComment.getText().toString(), getApplicationContext());
 		returnTakeMediaActivity();
 	}
 	
 	
-	private void gestionHistoriquePreferences(){
-		
-		SharedPreferences sharedpreferences  =  getSharedPreferences("historiquePreferences", MODE_PRIVATE);
-		
-		String stKey;
-		String stValue = "";
-		
-	
-		//Decalage Historique 
-		
-		Editor editor = sharedpreferences.edit();
-		for (int i = 9;i >0;i--)
-		{
-			stKey= "filePath" + String.valueOf(i);
-			stValue = sharedpreferences.getString(stKey, "%&%p%&defValue");
-			
-			if (!stValue.equals("%&%p%&defValue"))
-			{
-				stKey= "filePath" + String.valueOf(i+1);
-				editor.putString(stKey,stValue);	
-			
-				stKey= "commentaire" + String.valueOf(i);
-				stValue = sharedpreferences.getString(stKey, "%&%p%&defValue");
-				
-				stKey= "commentaire" + String.valueOf(i+1);
-				editor.putString(stKey,stValue);	
-				
-			}
-		}
-		//insertion nouvelles donnees
-		stKey= "filePath1";
-		stValue = photoUri.toString();
-		editor.putString(stKey,stValue);	
-		
-		stKey= "commentaire1";
-		stValue = editTextComment.getText().toString();
-		editor.putString(stKey,stValue);	
-		
-		editor.commit();
-
-	}
-	
+//	private void gestionHistoriquePreferences(){
+//		
+//		SharedPreferences sharedpreferences  =  getSharedPreferences("historiquePreferences", MODE_PRIVATE);
+//		
+//		String stKey;
+//		String stValue = "";
+//		
+//	
+//		//Decalage Historique 
+//		
+//		Editor editor = sharedpreferences.edit();
+//		for (int i = 9;i >0;i--)
+//		{
+//			stKey= "filePath" + String.valueOf(i);
+//			stValue = sharedpreferences.getString(stKey, "%&%p%&defValue");
+//			
+//			if (!stValue.equals("%&%p%&defValue"))
+//			{
+//				stKey= "filePath" + String.valueOf(i+1);
+//				editor.putString(stKey,stValue);	
+//			
+//				stKey= "commentaire" + String.valueOf(i);
+//				stValue = sharedpreferences.getString(stKey, "%&%p%&defValue");
+//				
+//				stKey= "commentaire" + String.valueOf(i+1);
+//				editor.putString(stKey,stValue);	
+//				
+//			}
+//		}
+//		//insertion nouvelles donnees
+//		stKey= "filePath1";
+//		stValue = photoUri.toString();
+//		editor.putString(stKey,stValue);	
+//		
+//		stKey= "commentaire1";
+//		stValue = editTextComment.getText().toString();
+//		editor.putString(stKey,stValue);	
+//		
+//		editor.commit();
+//
+//	}
+//	
 	private void returnTakeMediaActivity(){
 		
 		Intent intent = new Intent(this, TakeMediaActivity.class);
