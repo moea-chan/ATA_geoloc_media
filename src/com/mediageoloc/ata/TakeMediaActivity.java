c891907b682f403f584e6916f15624d5c9ed4d02
Anatol Ursu <uanatol@yahoo.com>
2014-10-28 12:17:09 +0100

    Strange conflits fixed.

--- src/com/mediageoloc/ata/TakeMediaActivity.java
+++ src/com/mediageoloc/ata/TakeMediaActivity.java
@@ -16,7 +16,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
-@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class TakeMediaActivity extends DrawerActivity {
 	
 
