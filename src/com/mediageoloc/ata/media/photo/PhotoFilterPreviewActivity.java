90bf3489bdea69876a3bd08ae1ce3fdf69a99756
Thierry Puch <t.puch@laposte.net>
2014-10-29 07:00:08 +0100

    Modif photo

--- src/com/mediageoloc/ata/media/photo/PhotoFilterPreviewActivity.java
+++ src/com/mediageoloc/ata/media/photo/PhotoFilterPreviewActivity.java
@@ -2,8 +2,8 @@ package com.mediageoloc.ata.media.photo;
 
 import java.io.IOException;
 
-
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
@@ -38,13 +38,7 @@ public class PhotoFilterPreviewActivity extends Activity {
 		imageView = (ImageView) findViewById(R.id.photo_preview);
 		photoUri = Uri.parse(intent.getStringExtra("photoUri"));
 
-		try {
-			Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),photoUri);
-			bitmap = Bitmap.createScaledBitmap(bitmap, 140, 190, false);
-			imageView.setImageBitmap(bitmap);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		chargeImageSourcePreview();
 	}
 
 	private void addButtonGoToCommentpreviewListener(){
@@ -56,6 +50,17 @@ public class PhotoFilterPreviewActivity extends Activity {
 			}
 		});
 	}
+	
+	private void chargeImageSourcePreview() {
+		try {
+			Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),photoUri);
+			bitmap = Bitmap.createScaledBitmap(bitmap, 140, 190, false);
+			imageView.setImageBitmap(bitmap);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	private void addButtonFilterListener(){
 		filterAction = (CheckBox) findViewById(R.id.check_box_filter);
 		filterAction.setOnClickListener(new View.OnClickListener() {
@@ -72,52 +77,34 @@ public class PhotoFilterPreviewActivity extends Activity {
 		startActivity(intent);
 	}
 
-	public void doFilter(){
+	private void doFilter(){
 		if (filterAction.isChecked()){
 			int value=100;
 
 			BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
 			final Bitmap src = drawable.getBitmap();
-
-			// image size
-			int width = src.getWidth();
-			int height = src.getHeight();
-			// create output bitmap
-			Bitmap bmOut = Bitmap.createBitmap(width, height, src.getConfig());
-			// color information
-			int A, R, G, B;
-			int pixel;
-
-			// scan through all pixels
-			for(int x = 0; x < width; ++x) {
-				for(int y = 0; y < height; ++y) {
-					// get pixel color
-					pixel = src.getPixel(x, y);
-					A = Color.alpha(pixel);
-					R = Color.red(pixel);
-					G = Color.green(pixel);
-					B = Color.blue(pixel);
-
-					// increase/decrease each channel
-					R += value;
-					if(R > 255) { R = 255; }
-					else if(R < 0) { R = 0; }
-
-					G += value;
-					if(G > 255) { G = 255; }
-					else if(G < 0) { G = 0; }
-
-					B += value;
-					if(B > 255) { B = 255; }
-					else if(B < 0) { B = 0; }
-
-					// apply new pixel color to output bitmap
-					bmOut.setPixel(x, y, Color.argb(A, R, G, B));
-				}
-			}
-
-			// return final image
+			Bitmap bmOut = PhotoUtils.brightnessFilter(value, src);
 			imageView.setImageBitmap(bmOut);
 		}
+		else{
+			chargeImageSourcePreview();
+		}
 	}
+/**
+ * saveFilteredPictures : Load original picture to apply filter before save it
+ */
+	private void saveFilteredPictures(){
+		if (filterAction.isChecked()){
+			int value=100;
+			try {
+				final Bitmap src = MediaStore.Images.Media.getBitmap(this.getContentResolver(),photoUri);
+				Bitmap bmOut = PhotoUtils.brightnessFilter(value, src);
+				//MediaStore.Images.Media.insertImage(, source, title, description)
+				//TODO/ Faire sauvegarde disque
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 }
