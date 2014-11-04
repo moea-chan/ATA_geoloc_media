e04fa756daf3dc87d82aa6044387795ab9b43e4a
Thierry Puch <t.puch@laposte.net>
2014-11-04 16:30:51 +0100

    aaa

---
+++ src/com/mediageoloc/ata/user/UserFollowActivity.java
@@ -0,0 +1,33 @@
+package com.mediageoloc.ata.user;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.mediageoloc.ata.R;
+import com.mediageoloc.ata.R.layout;
+import com.mediageoloc.ata.bddsqlite.Collaborator;
+import com.mediageoloc.ata.bddsqlite.GeolocMediaDbReader;
+
+/**
+ * Liste des personnes suivies (Temporairement tables des collaborators
+ * @author Thierry
+ *
+ */
+public class UserFollowActivity extends Activity {
+
+	ArrayList<Collaborator> arrayListUsersFollow;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_user_follow);
+		
+		//Chargement de la table Collaborator
+		arrayListUsersFollow = GeolocMediaDbReader.readListCollaboratorsDB(getApplicationContext());
+		
+	
+	}
+	
+	
+}
