32eee75648d0705ff259b84a9cb4038d8781c7cd
Moea-Chan <audeakrich17@hotmail.fr>
2014-11-05 16:26:22 +0100

    implementation du loader manager affichage de marker sur la map pour les 10 1ers users github de la db

--- src/com/mediageoloc/ata/map/MapActivity.java
+++ src/com/mediageoloc/ata/map/MapActivity.java
@@ -1,37 +1,81 @@
 package com.mediageoloc.ata.map;
 
-import android.app.FragmentTransaction;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.mediageoloc.ata.R;
-import com.mediageoloc.ata.R.id;
-import com.mediageoloc.ata.R.layout;
 import com.mediageoloc.ata.drawer.DrawerActivity;
+import com.mediageoloc.ata.user.UserProvider;
+import com.mediageoloc.ata.utils.GitHubService;
+import com.mediageoloc.ata.utils.MediaGeolocContract.Users;
 
-public class MapActivity extends DrawerActivity {
+public class MapActivity extends DrawerActivity implements LoaderCallbacks<Cursor>{
+	private GoogleMap mMap;
+	private MapFragment fragment;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
+		fragment = (MapFragment)getFragmentManager().findFragmentById(R.id.map);
+	}
+	
+	@Override
+	protected void onResume(){
+		super.onResume();
+		mMap = fragment.getMap();
+		getUsers();
+	}
+	
+	//for every user found in db create a marker on map
+	private void mapUsersToMarkers(Cursor cursor) {
+		cursor.moveToFirst();
 		
-		GoogleMapOptions options = new GoogleMapOptions();
+		Marker marker;
+		String pseudo;
+		
+		while (!cursor.isAfterLast()) {
+			pseudo = cursor.getString(cursor.getColumnIndexOrThrow(Users.COLUMN_NAME_PRENOM));
+			marker = mMap.addMarker(new MarkerOptions()
+			.position(new LatLng(Math.random()*100, Math.random()*100))
+			.title(pseudo));
+		    cursor.moveToNext();
+		}
+		cursor.close();
+	}
+	
+	//get github user and init loader
+	private void getUsers() {
+		// init github service to get all users
+		GitHubService service = new GitHubService();
+		service.getUsers(getApplicationContext());
+		//get loader or create it
+		getLoaderManager().initLoader(0, null, this);
+	}
 
-		options.mapType(GoogleMap.MAP_TYPE_SATELLITE)
-		    .compassEnabled(false)
-		    .rotateGesturesEnabled(false)
-		    .tiltGesturesEnabled(false);
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		//create loader 
+		String[] projection = {Users.COLUMN_NAME_PRENOM};
+		return new CursorLoader(this,
+				UserProvider.CONTENT_URI, projection, null, null, null);
+	}
 
-		MapFragment fragment = MapFragment.newInstance(options);
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+		mapUsersToMarkers(data);
+	}
 
-		FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-
-		fragmentTransaction.add(R.id.map, fragment);
-
-		fragmentTransaction.commit();
-
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
+		// TODO Auto-generated method stub
 	}
 }
--- src/com/mediageoloc/ata/user/FollowersActivity.java
+++ src/com/mediageoloc/ata/user/FollowersActivity.java
@@ -1,9 +1,11 @@
 package com.mediageoloc.ata.user;
 
 import android.app.Activity;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v4.content.CursorLoader;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.widget.ListView;
 import butterknife.ButterKnife;
@@ -13,7 +15,7 @@ import com.mediageoloc.ata.R;
 import com.mediageoloc.ata.utils.GitHubService;
 import com.mediageoloc.ata.utils.MediaGeolocContract.Users;
 
-public class FollowersActivity extends Activity {
+public class FollowersActivity extends Activity implements LoaderCallbacks<Cursor> {
 
 	@InjectView(R.id.followers_list)
 	ListView followersViewList;
@@ -29,6 +31,11 @@ public class FollowersActivity extends Activity {
 		GitHubService service = new GitHubService();
 		service.getUsers(getApplicationContext());
 		
+		getLoaderManager().initLoader(0, null, this);
+	}
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
 		String[] projection = {
 			    Users._ID,
 			    Users.COLUMN_NAME_NOM,
@@ -36,17 +43,21 @@ public class FollowersActivity extends Activity {
 			    Users.COLUMN_NAME_MAIL,
 			    Users.COLUMN_NAME_TELEPHONE
 			    };
-		
+		return new CursorLoader(this,
+				   UserProvider.CONTENT_URI, projection, null, null, null);
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
 		String[] fromColumns = new String[] { Users.COLUMN_NAME_PRENOM, Users.COLUMN_NAME_TELEPHONE };
 		int[] toControlIds = new int[] { R.id.follower_item, R.id.follower_picture };
 
-		CursorLoader loader = new CursorLoader(this,
-		   UserProvider.CONTENT_URI, projection, null, null, null);
-		Cursor cursor = loader.loadInBackground();
-		// create a SimpleCursorAdapter
-		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.activity_follower_item, cursor, fromColumns, toControlIds);
+		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.activity_follower_item, data, fromColumns, toControlIds);
 		followersViewList.setAdapter(adapter);
 	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {}
 	
 	
 }
