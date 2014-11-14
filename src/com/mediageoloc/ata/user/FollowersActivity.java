98ca776bfb6e4916138fe1e093ca2d66cc7c454b
Thierry Puch <t.puch@laposte.net>
2014-11-14 16:19:44 +0100

    avec followers

---
+++ src/com/mediageoloc/ata/user/FollowersActivity.java
@@ -0,0 +1,56 @@
+package com.mediageoloc.ata.user;
+
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.widget.ListView;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+import com.mediageoloc.ata.R;
+import com.mediageoloc.ata.drawer.DrawerActivity;
+import com.mediageoloc.ata.utils.MediaGeolocContract.Users;
+
+public class FollowersActivity extends DrawerActivity implements LoaderCallbacks<Cursor> {
+
+	@InjectView(R.id.followers_list)
+	ListView followersViewList;
+		
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setDrawerContentView(R.layout.activity_followers);
+		
+		ButterKnife.inject(this);
+	
+		// init github service to get all users
+		GitHubService service = new GitHubService();
+		service.getUsers(getApplicationContext());
+		
+		getLoaderManager().initLoader(0, null, this);
+	}
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		return new CursorLoader(this,
+				   UsersProvider.FOLLOWERS_CONTENT_URI, null, null, null, null);
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+		String[] fromColumns = new String[] { Users.COLUMN_NAME_PRENOM, Users.COLUMN_NAME_TELEPHONE };
+		int[] toControlIds = new int[] { R.id.follower_item, R.id.follower_picture };
+
+		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.follower_item, data, fromColumns, toControlIds);
+		followersViewList.setAdapter(adapter);
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {}
+	
+	
+}
+
